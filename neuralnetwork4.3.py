3dc4af7579d3b46965b64337644d1fd40d5226d7
¨JeremyRuizenaar¨ <jeremy.ruizenaar@student.hu.nl>
2017-12-06 03:17:44 +0100

    save commt nor with learning

--- neuralnetwork4.3.py
+++ neuralnetwork4.3.py
@@ -3,9 +3,11 @@ import random
 
 class Neuron:
 
-    def __init__(self, weights, treshold):
-        self.treshold = treshold
+    def __init__(self, weights, bias):
+        self.bias = bias
         self.weights = weights
+        self.oldWeights = [0 for x in range(0, len(self.weights))]
+
 
     def getWeights(self):
         return self.weights
@@ -19,42 +21,66 @@ class Neuron:
 
         return result
 
+    def subtractList(self, a , b):
+        result = []
+        i = 0
+        for x in range(0, len(a)):
+            #print(a[i])
+            #print(b[i])
+            res =  (float(a[i]) - float(b[i]))
+            #print("result of subtract list = ", res)
+            result.append(res)
+            i += 1
+        return result
+
     def update(self, data ,inputSum, actual, desired):
         print("updating neuron")
-        #print("inputSum = ", inputSum)
-        #print("actual = ", actual)
-        #print("desired = ", desired)
-        learnRate = 0.25
+
+        self.oldWeights = self.weights[:]
+        learnRate = 0.01
 
         for x in range(0, len(self.weights)):
-            #print("delta weight = ", (learnRate * data[x] * (1 / self.sigmoid(inputSum)) * (desired - actual)))
-            print("delta weight = ", learnRate * (desired - actual) * data[x])
-            #self.weights[x] += float(learnRate * data[x] * (1 / self.sigmoid(inputSum)) * (desired - actual))
-            self.weights[x] += learnRate * (desired - actual) * data[x]
+
+            errorSig = ( desired - actual ) *  actual * ( 1 - actual )
+            #print("errorSig = ", errorSig)
+            tmp = float(learnRate * errorSig *   data[x])
+            #print("tmp = ", tmp)
+
+            self.weights[x] += tmp
+
+
+        # a = self.subtractList(self.oldWeights, self.weights)
+        # # print(a)
+        # if sum(a) <= 0.01:
+        #     print("converged")
+        #     # return True
 
         print("new weights = " ,self.weights)
         print("-" * 50)
         print()
+
     def sigmoid(self, x):
         return 1 / (1 + exp(-x))
 
     def act(self, input):
-        #print("sigmoid function test = ",self.sigmoid(0))
-        #print("act() invoked")
-        #print("act input = ", input)
-        #print("res of sumweights = ", sum(self.weightMultiplier(input)))
-        res = self.sigmoid(sum(self.weightMultiplier(input)))
-        #print("result of think = " , res)
+        #bias = 3
+        res = self.sigmoid(sum(self.weightMultiplier(input)) + self.bias)
         return  res
 
 class Network:
 
-    def __init__(self):
+    def __init__(self, it):
         #self.norGate = Neuron([random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1)], 0)
-        self.trainingSet = [[0, 0, 0, 0], [0, 0, 0,1], [0, 1, 0, 0], [0,1, 0, 0], [0, 1, 0, 1], [1, 1, 1, 0], [1, 0, 1, 0], [1, 1, 1, 1]]
-        #self.trainingAnswer = [1, 0, 0, 0, 0, 0, 0, 0]
-        self.trainingAnswer = [0, 1, 1, 1, 1, 1, 1, 1]
-        self.norGate = Neuron([random.uniform(0, 1) for x in range(0, len(self.trainingSet[0]))], 0)
+        self.iterations = it
+        # self.trainingSet = [[0, 0, 0], [0, 0, 1], [0, 1, 0], [0, 1, 1], [1, 0, 0],[1, 0, 1], [1, 1, 0] ,[1,1, 1]]
+        # self.trainingAnswer = [1, 0, 0, 0, 0, 0, 0, 0]
+
+        self.trainingSet = [[0, 0, 0, 1], [0, 0, 0, 0], [0, 1, 0, 0], [0,0, 1, 0], [1, 0, 0, 0]]
+        self.trainingAnswer = [0, 1, 0, 0, 0]
+        #self.trainingAnswer = [1, 0, 1, 1,1]
+
+
+        self.norGate = Neuron([random.uniform(0, 1) for x in range(0, len(self.trainingSet[0]))], 3)
 
     def calculateDistance(self, a, b):
         # calcululate difference between two classes represented by two lists
@@ -71,21 +97,30 @@ class Network:
         print("training")
         print()
 
-        for x in range(0,50):
-            print("-"*50)
+        for x in range(0, self.iterations):
+            print("-"*25, x , "-"*25)
             i = 0
+            cumError = 0
+
             for data in self.trainingSet:
                 print("desired result = ", self.trainingAnswer[i])
+
                 result = float(self.think(data))
                 print("result = ", result)
-                if( self.trainingAnswer[i]) - result < 0.10:
-                    #cost function berekenen ofzoiets
-                    print("accurate")
-                    #return
 
-                self.norGate.update( data , sum(data) , result , self.trainingAnswer[i] )
+                cumError += math.pow((self.trainingAnswer[i] - result), 2)
+                print("cumError() = ", cumError)
+
+                if self.norGate.update( data , sum(data), result , self.trainingAnswer[i] ) == True:
+                    print("done traing")
+                    return
                 i += 1
 
+            if cumError < 0.01:
+                print("cum error is low")
+                return True
+
+            print("cumError(total) = ", cumError)
 
 
 
@@ -98,7 +133,7 @@ class Network:
 
 
 
-a = Network()
+a = Network(1000000)
 a.showWeights()
 a.train()
 print(a.think([0,0,0,0]))#
